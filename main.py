16d0d646d89fb40318be2579d6e2e8cf31146803
Calvin.R <calvin.2000@outlook.com>
2022-02-09 16:39:04 +0000

    download funtionality (testing)

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from email import message
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-from flask import Blueprint
-from .models import flags
+from flask import Blueprint, send_file, send_from_directory, safe_join, abort
+import os
+from .models import Games, Flags
 from flask_login import login_required, current_user
+from . import db
 
 
 main = Blueprint('main', __name__)
@@ -16,7 +18,16 @@ def index():
 @main.route("/gameselect")
 @login_required
 def gameselect():
-    return render_template("gameselect.html")
+    return render_template("gameselect.html", games=flags.query.all())
+
+
+@main.route("/gameselect/<path:ctf_name>", methods=['GET', 'POST'])
+@login_required
+def download(ctf_name):
+    try:
+        return send_from_directory("/mnt/c/Users/Calvin/CTFBuddy/static/ctfs", filename=ctf_name, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
 
 @main.route('/profile')
 @login_required
