729f901bc5754e3cb7dfe3e34f0fb55ac99b4262
Calvin.R <calvin.2000@outlook.com>
2022-03-21 00:00:25 +0000

    funtion to upload a video

--- create.py
+++ create.py
@@ -4,7 +4,8 @@ from .models import Games, Flags
 from flask_login import login_required, current_user
 from . import db
 from werkzeug.utils import secure_filename
-
+from urllib.parse import urlparse, parse_qs
+from contextlib import suppress
 
 create = Blueprint('create', __name__)
 
@@ -62,6 +63,8 @@ def create_post():
     description = request.form.get('description')
     category = request.form.get('category')
     difficulty = request.form.get('difficulty')
+    video = request.form.get('video')
+    #video_cost = request.form.get('video_cost')
 
     flagstrings = ['flag1', 'flag2', 'flag3', 'flag4',
                    'flag5', 'flag6', 'flag7', 'flag8', 'flag9', 'flag10']
@@ -69,6 +72,19 @@ def create_post():
                          'flag5_hint', 'flag6_hint', 'flag7_hint', 'flag8_hint', 'flag9_hint', 'flag10_hint']
     flag_point_strings = ['points1', 'points2', 'points3', 'points4',
                          'points5', 'points6', 'points7', 'points8', 'points9', 'points10']
+    #hint_cost_strings = ['hintcost1','hintcost2','hintcost3','hintcost4','hintcost5','hintcost6','hintcost7','hintcost8','hintcost9','hintcost10']
+
+    query = urlparse(video)
+    if query.hostname == 'youtu.be': return query.path[1:]
+    if query.hostname in {'www.youtube.com', 'youtube.com', 'music.youtube.com'}:
+        
+        if query.path == '/watch': video = parse_qs(query.query)['v'][0]
+        if query.path[:7] == '/watch/': video = query.path.split('/')[1]
+        if query.path[:7] == '/embed/': video = query.path.split('/')[2]
+        if query.path[:3] == '/v/': video = query.path.split('/')[2]
+    else:
+        flash('Invalid Youtube Link.')
+        return redirect(url_for('main.create'))
     
     if not name or not description or not category or not difficulty:
         flash('Not enough general information provided.')
@@ -81,10 +97,12 @@ def create_post():
         return redirect(url_for('main.create'))
     
     flag_num = 0
+    total_points = 0
     while flag_num < 10:
         flag = request.form.get(flagstrings[flag_num])
         flag_hint = request.form.get(flag_hint_strings[flag_num])
-        flag_points = request.form.get(flag_point_strings[flag_num])
+        flag_points = request.form.get(flag_point_strings[flag_num], type=int)
+        #hint_cost = request.form.get(hint_cost_strings[flag_num])
         if flag:
             if not flag_hint:
                 flash('Hint required.')
@@ -92,7 +110,8 @@ def create_post():
             if not flag_points:
                 flash('Points required.')
                 return redirect(url_for('main.create'))
-            new_flag = Flags(flag_num=flag_num, flag=flag, hint=flag_hint,
+            total_points += flag_points
+            new_flag = Flags(flag_num=flag_num, flag=flag, hint=flag_hint, hint_cost=flag_points // 2,
                              points=flag_points, games_name=name)
             db.session.add(new_flag)
             flag_num += 1
@@ -108,9 +127,16 @@ def create_post():
         if not filename:
             flash('Error with upload.')
             return redirect(url_for('main.create'))
+    
+    if not video:
+        flash('No video url provided.')
+        return redirect(url_for('main.create'))
+    
 
-    new_game = Games(name=name, category=category,
-                     difficulty=difficulty, url=filename, user_id=current_user.id)
+    new_game = Games(name=name,description=description, category=category,
+                     difficulty=difficulty, url=filename, video_url=video, video_cost=round((total_points*0.75)/10)*10, user_id=current_user.id)
+    
+    current_user.points += 1000
     
     file = request.files["file"]
     file.save(os.path.join(current_app.config["FILE_UPLOADS"], filename))
